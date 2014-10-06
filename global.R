c0f8414f31e10cb3e857511c3959c0b5a9458c5e
jhuang <jhuang@dev1.localdomain>
2014-10-05 17:11:56 -0700

    cleanup global.R

--- global.R
+++ global.R
@@ -3,7 +3,9 @@ library(ggplot2)
 library(maps)
 library(shiny)
 
-# data all powerplants (pows)
+# data: all powerplants (pows)
+# add goMap links to the data. This code should be only run once, and then save
+# the results as *rda, which can be loaded much faster. 
 # setwd('C:/Users/jianhua/Dropbox/test/shiny/powerplants')
 # pows <- read.csv('C:/Users/temp/Dropbox/test/shiny/powerplants/eGRID2012V1_0_year09_DATA.csv')
 # Go <- apply(pows, 1, function(x) paste0(tags$button("goMap", onclick = paste0("return (loc = ", x["ORISPL"], ")"))))
@@ -12,11 +14,7 @@ library(shiny)
 
 load('eGRID2012V1_0_year09_DATA.rda')
 
-## select only the CAMD powerplants
-# pows <- pows[pows$SOURCEM == 'EPA CAMD', ]
-
-# pows <- pows[sample(5000, 200), ]
-## match the fuel category with displayed name and point color in the map
+# match the fuel category with displayed name and point color in the map
 categ <- data.frame(
   PLFUELCT = c("COAL", "OFSL", "OIL", "GAS", ""),
   FUELPOPUP = c('Coal', 'Oil/Gas/Other fossil fuel', 'Oil/Gas/Other fossil fuel', 'Oil/Gas/Other fossil fuel', 'Renewable'),
@@ -24,17 +22,17 @@ categ <- data.frame(
 )
 power.categ <- categ[match(pows$PLFUELCT, categ$PLFUELCT), 2:3] 
 
-## select the columns we want to display
-## AN means annual emission, RTA means annual emission rate (lb/MWh) or (lb/GWh)
-## all HG values are NA, so exclude the emission of HG
+# select the columns we want to display
+# AN means annual emission, RTA means annual emission rate (lb/MWh) or (lb/GWh)
+# all HG values are NA, so exclude the emission of HG
 phy.info <- c('PNAME', 'ORISPL', 'PSTATABB', 'CNTYNAME', 'LAT', 'LON', 'PLPRMFL', 'PLNGENAN')
 em <- c('PLNOXAN', 'PLSO2AN', 'PLCO2AN', 'PLCH4AN', 'PLN2OAN')
 emr <- c('PLNOXRTA', 'PLSO2RTA', 'PLCO2RTA', 'PLCH4RTA', 'PLN2ORTA')
 
 pows <- pows[,c(phy.info, em, emr, 'Go')]
 
-## the state coordinates, which will be jumped to once the state is selected
-## var.states is the choice of selectInput
+# the state coordinates, which will be jumped to once the state is selected
+# var.states is the choice of selectInput
 state.coord <- read.csv('state_latlon.csv', stringsAsFactors = F)
 state.abbr <- read.csv('states.abbr.csv', stringsAsFactors = F)
 states <- merge(state.abbr, state.coord, by.x = 'Abbreviation', by.y = 'state')
